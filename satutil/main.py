ee7f5155046e4b29316161783fb64dd70d023aca
Matthew Hanson <matt.a.hanson@gmail.com>
2016-06-17 23:04:44 -0400

    move script logic to main module

--- satutil/main.py
+++ satutil/main.py
@@ -1,12 +1,33 @@
 from .parser import SatParser
 from .version import __version__
-from ssearch import Search
+#
+
+'''
+    This is a generic command line program for processing geospatial raster data.
+    Use this as a template in data-specific sat-util based utilities.
+'''
+
 
 def main():
-    title = 'Sat-util CLI'
-    try:
-        parser = SatParser(description=title)
-        if args.subs == 'search':
-            results = Search.query(**args)
-        elif args.subs == 'download:'
-            results = Search.query(**args)
+    #title = 'Sat-util CLI'
+
+    parser = SatParser()
+    args = parser.parse_args()
+    
+    print args
+
+    #if args.subs == 'search':
+    #    results = Search.query(**args)
+    #elif args.subs == 'download':
+    #    results = Search.query(**args)
+
+    #if args.indir is not None:
+        # read scene from directory
+    #    scene = Scene.create_from_directory(args.indir)
+        # 1 - collect product arguments
+        # 2 - generate output filenames
+        # 3 - call scene.process(products)
+    #else:
+    #    for sid in args.sceneids:
+    #        fname = os.path.join(args.indir, sid)
+    #        scene = Scene(fname)
